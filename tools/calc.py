c160a5d4f06da66ca492d715a22392bfd00cf248
JoshPaulie (Bexli) <30572280+JoshPaulie@users.noreply.github.com>
2023-04-30 09:25:01 -0500

    📦 Move hours worked today logic to func

--- tools/calc.py
+++ tools/calc.py
@@ -10,23 +10,20 @@ shift_start_time = dt.time(9, 0, 0)
 shift_end_time = dt.time(16, 0, 0)
 shift_len = shift_end_time.hour - shift_start_time.hour
 
-# Hours worked today
-shift_start = dt.datetime.combine(today, shift_start_time)
-hours_since_shift_start = (now - shift_start).seconds / 60 / 60
-
-# fyi - bc the tool might be used *after* work hours, this value caps at 7
-hours_worked_today = round(hours_since_shift_start) if hours_since_shift_start < shift_len else shift_len
-
-# fyi - keeps tool from breaking if checked before work hours
-# fyi - keeps tool from giving inaccurate time if checked over the weekend
-if now.time() < shift_start_time or now.date().weekday() in [5, 6]:
-    hours_worked_today = 0
-
 
 # Functions 🧠
-def calc_days() -> int:
-    """Find days between today -> last day in the contract, today, & weekends inclusive"""
-    return (last_day - today).days + 1
+def hours_worked_today():
+    # fyi - keeps tool from breaking if checked before work hours
+    if now.time() < shift_start_time:
+        return 0
+
+    shift_start = dt.datetime.combine(today, shift_start_time)
+    hours_since_shift_start = (now - shift_start).seconds / 60 / 60
+
+    # fyi - bc the tool might be used *after* work hours, this value caps at 7
+    hours_worked_today = round(hours_since_shift_start) if hours_since_shift_start < shift_len else shift_len
+
+    return hours_worked_today
 
 
 def is_weekend(date: dt.date) -> bool:
